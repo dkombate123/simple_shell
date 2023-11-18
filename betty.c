9c32c6631c7bf4443dc06910a9c9696e271d599d
KOMBATE DANDIEGOU <kdandiegou@gmail.com>
2023-11-18 11:27:07 +0300

    Create betty.c

---
+++ betty.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MAX_NUM 10
+/**
+ *  print_message - check the code
+ *
+ * Return: nothing
+ */
+void print_message(void)
+{
+	printf("Hello, Betty!\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_message();
+	return (0);
+}
